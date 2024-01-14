0dd914afd92a295db9de367d3e18619e94a03d16
WojD89 <wojciech.dyrka@gmail.com>
2024-01-14 12:32:36 +0100

    imports and files order

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,8 @@
+import Profile from '.Profile/';
+import Statistics from './Statistics';
+import FriendList from './FriendList';
+import TransactionHistory from './TransactionHistory';
+
 export const App = () => {
   return (
     <div
