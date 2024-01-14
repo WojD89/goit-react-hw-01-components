01cda928750a14510a5ab9b95f702eb688c14846
WojD89 <wojciech.dyrka@gmail.com>
2024-01-14 12:59:46 +0100

    profile done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
-import Profile from '.Profile/';
-import Statistics from './Statistics';
-import FriendList from './FriendList';
-import TransactionHistory from './TransactionHistory';
+import Profile from './Profile/Profile';
+//import Statistics from './Statistics';
+//import FriendList from './FriendList';
+//import TransactionHistory from './TransactionHistory';
+
+import user from './Profile/user.json';
 
 export const App = () => {
   return (
@@ -15,7 +17,13 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+/>
     </div>
   );
 };
